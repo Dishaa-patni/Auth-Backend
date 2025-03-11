07f753aeb5f37340ae4cfaf55ca93f2eb8ce96a2
Dishaa Patni <dishaapatni8@gmail.com>
2025-03-11 21:28:32 +0530

    Set-up for backend

---
+++ server.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv'
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+const app=express();
+
+app.use(express.json());
+
+app.use(cors());
+
+dotenv.config();
+
+const connectToDb=()=>{
+    mongoose.connect(process.env.MONGO_DB_URL)
+    .then(()=>console.log("MongoDb connected"))
+    .catch(err=>console.log(err));
+}
+
+connectToDb();
+app.listen(5000,()=>console.log("The server is running on port 5000"));
